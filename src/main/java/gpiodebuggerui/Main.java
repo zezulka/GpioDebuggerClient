f961326668a1782b3a2605d51841d6b5793d602d
miloslav <miloslav@pc>
2017-02-18 18:26:25 +0100

    init state

---
+++ src/main/java/gpiodebuggerui/Main.java
@@ -0,0 +1,59 @@
+package gpiodebuggerui;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+import net.ConnectionManager;
+
+/**
+ *
+ * @author Miloslav Zezulka, 2017
+ */
+public class Main {
+
+    private static Socket sock;
+    private static final Scanner MOCK_INPUT = new Scanner(System.in);
+    private static PrintStream OUTPUT;
+    private static BufferedReader INPUT;
+    private static boolean hasFinished = false;
+
+    public static void main(String[] args) {
+        try {
+            initResources();
+            System.out.println("Connection to server OK");
+            while (!hasFinished) {
+                sendRequest(MOCK_INPUT.nextLine());
+                receiveResponse();
+            }
+            sock.close();
+        } catch (IOException ex) {
+            System.err.println("Error has occured: \n" + ex);
+            ex.printStackTrace(System.err);
+        }
+    }
+    
+    private static void initResources() throws IOException {
+        sock = new Socket("10.42.0.138", ConnectionManager.DEFAULT_SOCK_PORT);
+        INPUT = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        OUTPUT = new PrintStream(sock.getOutputStream());
+    }
+
+    private static void receiveResponse() throws IOException {
+        String line;
+        while ((line = INPUT.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+    private static void sendRequest(String mock_request) throws IOException {
+        if ("exit".equals(mock_request)) {
+            hasFinished = true;
+            return;
+        }
+        OUTPUT.println(mock_request);
+    }
+}
